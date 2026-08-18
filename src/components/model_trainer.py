6c6e68eef4d0ca078d09a0492b98a7b6ef7884bf
Harshita6097 <harshita6097@gmail.com>
2026-08-18 17:19:13 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,43 +48,44 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
             
-            # params={
-            #     "Decision Tree": {
-            #         'criterion':['squared_error', 'absolute_error', 'poisson'],
-            #         'max_features':['sqrt','log2',None],
-            #         'max_depth': [3,5,7,10]
-            #     },
-            #     "Random Forest":{
-            #         'n_estimators': [8,16,32,64,128,256],
-            #         'max_features': ['sqrt','log2']
-            #     },
-            #     "Gradient Boosting":{
-            #         'learning_rate':[.1,.01,.001,.05,.005,.0001],
-            #         'n_estimators': [8,16,32,64,128,256]
-            #     },
-            #     "K-Neighbors Regressor":{
-            #         'n_neighbors': [5,7,9,11],
-            #         'weights': ['uniform','distance'],
-            #         'algorithm': ['ball_tree','kd_tree','brute']
-            #     },
-            #     "XGBRegressor":{
-            #         'n_estimators': [8,16,32,64,128,256],
-            #         'learning_rate': [.1,.01,.001,.0001],
-            #         'max_depth': [3,5,7,9]
-            #     },
-            #     "CatBoosting Regressor":{
-            #         'iterations': [8,16,32,64,128,256],
-            #         'learning_rate': [.1,.01,.001,.0001],
-            #         'depth': [3,5,7,9]
-            #     },
-            #     "AdaBoost Regressor":{
-            #         'learning_rate': [.1,.01,.001,.0001, .00001],
-            #         'n_estimators': [8,16,32,64,128,256]
-            #     }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'absolute_error', 'poisson'],
+                    'max_features':['sqrt','log2',None],
+                    'max_depth': [3,5,7,10]
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256],
+                    'max_features': ['sqrt','log2']
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.001,.05,.005,.0001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors': [5,7,9,11],
+                    'weights': ['uniform','distance'],
+                    'algorithm': ['ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor":{
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate': [.1,.01,.001,.0001],
+                    'max_depth': [3,5,7,9]
+                },
+                "CatBoosting Regressor":{
+                    'iterations': [8,16,32,64,128,256],
+                    'learning_rate': [.1,.01,.001,.0001],
+                    'depth': [3,5,7,9]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate': [.1,.01,.001,.0001, .00001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
                 
-            # }
+            }
             
-            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             best_model_score = max(sorted(model_report.values()))
 
@@ -95,7 +96,7 @@ class ModelTrainer:
             best_model = models[best_model_name]
             
             if best_model_score<0.6:
-                raise CustomException("No best model found")
+                raise CustomException("No best model found", sys)
             logging.info(f"Best model found on both training and test dataset")
 
             save_object(
